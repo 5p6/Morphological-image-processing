c7640a470b527a912da609cc7a8e1b534fe08260
handsome boy <100855117+5p6@users.noreply.github.com>
2022-03-21 17:20:23 +0800

    Add files via upload

---
+++ dilate.m
@@ -0,0 +1,14 @@
+function f=ccor(p,h)
+[x,y]=size(p);
+p=im2double(p);
+f=zeros(x,y);
+for i=2:x-1
+    for j=2:y-1
+        g=p(i-1:i+1,j-1:j+1);
+        t=sum(sum(g.*h));
+        if t~=0
+            f(i,j)=1;
+        end
+    end
+end
+end
