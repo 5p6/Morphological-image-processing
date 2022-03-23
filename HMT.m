4791deab717b4778ae05ed5615ae0561404a313e
handsome boy <100855117+5p6@users.noreply.github.com>
2022-03-23 19:21:55 +0800

    Add files via upload

---
+++ HMT.m
@@ -0,0 +1,16 @@
+function f=HMT(g,h)
+g=im2double(g);
+[x,y]=size(g);
+f=zeros(x,y);
+g_s=zeros(x+2,y+2);
+g_s(2:x+1,2:y+1)=g;
+t=sum(sum(fix(h)));
+for i=2:x+1
+    for j=2:y+1
+        c=sum(sum(h.*g_s(i-1:i+1,j-1:j+1)));
+        if c==t
+            f(i-1,j-1)=1;
+        end
+    end
+end
+end
