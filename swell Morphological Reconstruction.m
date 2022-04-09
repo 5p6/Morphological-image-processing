70bf9e593a8a45c58c1d539e5de1c8f64892222f
handsome boy <100855117+5p6@users.noreply.github.com>
2022-04-09 14:59:49 +0800

    Update swell Morphological Reconstruction.m

--- swell Morphological Reconstruction.m
+++ swell Morphological Reconstruction.m
@@ -1,14 +1,12 @@
 function f=RD(F,G,b)
 i=0;
 while i<1
-     for j=1:5000
         X=F;
-        F=D(F,G,b,j);
+        F=D(F,G,b,1);
         if X==F
             i=i+1;
             break;
         end
-     end
 end
 f=X;
 end
